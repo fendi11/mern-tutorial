8340e8f28f154a80deaf1f30ff97be5f2883a0bd
fendi11 <driftmania64@gmail.com>
2022-07-05 11:22:25 +0700

    tambah server

--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,7 @@ const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 
 const app = express()
-
-app.listen(port, () => console.log('Server started on port ${port}'))
+app.get('/api/goals',(req,res)=>{
+    res.send({data :'Get Goals'})
+})
+app.listen(port, () => console.log(`Server started on port ${port}`))
