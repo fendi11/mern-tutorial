0e298d34bf6d9494c66c00488656f785d4326824
fendi11 <driftmania64@gmail.com>
2022-07-06 22:46:08 +0700

    menambahkan file route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,6 @@ const port = process.env.PORT || 5000
 
 const app = express()
 app.get('/api/goals',(req,res)=>{
-    res.send({data :'Get Goals'})
+    res.status(200).json({data :'Get Goals'})
 })
 app.listen(port, () => console.log(`Server started on port ${port}`))
