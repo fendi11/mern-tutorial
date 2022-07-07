78f92bfc4a0392b4e7d9177604b5e21ce772f284
fendi11 <driftmania64@gmail.com>
2022-07-07 09:34:51 +0700

    mendaftarkan route lain

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -3,4 +3,13 @@ const router = express.Router()
 router.get('/',(req,res)=>{
     res.status(200).json({data :'Get Goals'})
 })
+router.post('/',(req,res)=>{
+    res.status(200).json({message: 'SetGoal'})
+})
+router.put('/:id',(req,res)=>{
+    res.status(200).json({message: 'Update Goal '})
+})
+router.delete('/:id',(req,res)=>{
+    res.status(200).json({message: 'Delete Goal ${req.params.id}'})
+})
 module.exports = router
