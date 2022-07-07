a26b4e38ffd87970da485234e0f8415ca3cc55b2
fendi11 <driftmania64@gmail.com>
2022-07-07 12:26:34 +0700

    update goalroutes

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,27 @@
+//@desc Get Goals
+//@route GET /api/goals
+//@access Private
 const getGoals = (req,res) => {
     res.status(200).json({message: 'Get Goals'})
 }
+
+//@desc Set Goals
+//@route POST /api/goals
+//@access Private
 const setGoals = (req,res) => {
     res.status(200).json({message: 'SetGoal'})
 }
+
+//@desc update goals
+//@route PUT /api/goals/:id
+//@acces private
 const updateGoals = (req,res) => {
     res.status(200).json({message: 'Update Goal '})
 }
+
+//@desc delete goal
+//@route DELETE /api/goals/:id
+//@acces private
 const deleteGoals = (req,res) => {
     res.status(200).json({message: 'Delete Goal ${req.params.id}'})
 }
