d02afa3c8ad93a63122b124d9630f39b917f4084
fendi11 <driftmania64@gmail.com>
2022-07-09 19:34:19 +0700

    nodejs connect

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 console.log("Hello world")
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
 const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
+connectDB()
 const app = express()
 app.get('/api/goals',(req,res)=>{
     res.status(200).json({data :'Get Goals'})
