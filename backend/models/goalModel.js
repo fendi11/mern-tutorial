3c3313a0bed2c06a5426c01803e4c03ffd7f11cf
fendi11 <driftmania64@gmail.com>
2022-07-12 15:19:39 +0700

    penambahan middleware

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 const goalSchema = mongoose.Schema(
     {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "Users",
+          },
         text: {
             type: String,
             require: [true, 'please add a text value']
